ebcd9a49a9cf5f81f8349a976b060135febb9b85
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-22 18:11:43 +0600

    updated fileservice class

--- src/task3/util/FileService.java
+++ src/task3/util/FileService.java
@@ -2,25 +2,32 @@ package task3.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import task3.util.info.Movie;
-import task3.util.info.MovieCollection;
-
+import task3.info.Movie;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FileService {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final Path PATH = Paths.get("task1/data/movies.json");
+    private static final Path PATH = Paths.get("task3/data/movies.json");
 
 
-    public static void parseMovies(){
+    public static List<Movie> parseMovies(){
+        Map<String, List<Movie>> map = new HashMap<>();
+        Type itemsMapType = new TypeToken<Map<String, List<Movie>>>() {}.getType();
         String json = "";
         try{
-            json = GSON.fromJson(Files.readAllLines(PATH)).getMovies();
+            json = Files.readString(PATH);
         }catch (IOException e){
             System.out.println("Error " + e.toString());
         }
+        map = GSON.fromJson(json, itemsMapType);
+        return map.get("movies");
     }
 }
