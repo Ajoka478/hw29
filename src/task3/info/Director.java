32c1c60a29973bc920dbe17877cc798ae10745fa
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-22 18:11:29 +0600

    updated director class

--- src/task3/info/Director.java
+++ src/task3/info/Director.java
@@ -1,4 +1,23 @@
 package task3.info;
 
-public class Director {
+public class Director implements Comparable<Director>{
+    private String fullName;
+    public String getFullName(){
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Director = " + fullName;
+    }
+    @Override
+    public int compareTo(Director o) {
+        return this.getFullName().compareTo(o.getFullName());
+    }
+
 }
+
