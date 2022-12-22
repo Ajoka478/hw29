8e0de22af2576db3086c1b4c310c4700a018edf7
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-22 18:11:14 +0600

    updated Cast class

--- src/task3/info/Cast.java
+++ src/task3/info/Cast.java
@@ -1,4 +1,25 @@
 package task3.info;
 
 public class Cast {
+    private String fullName;
+    private String role;
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    @Override
+    public String toString() {
+        return String.format("Actor: %-10s | Role: %-10s", fullName, role);
+
+    }
 }
+
