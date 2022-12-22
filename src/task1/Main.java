3f96ba9c00cd416218aaadc6d1a8d4edb446b1ea
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-22 14:39:33 +0600

    updated main in task1

---
+++ src/task1/Main.java
@@ -0,0 +1,30 @@
+package task1;
+
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) {
+        sort();
+    }
+    private static void sort() {
+        var cats = Cat.makeCats(10);
+        System.out.println("|________Before sorting___________________|");
+        Printer.print(cats);
+
+        cats.sort(Cat::byBreed);
+        System.out.println("|________Sorting by breed___________________|");
+        Printer.print(cats);
+
+        cats.sort(Comparator.comparing(Cat::getName).thenComparing(Cat::getAge));
+        System.out.println("|________Sorting by name and age___________|");
+        Printer.print(cats);
+
+        System.out.println("|______After removing by color(Tabby)______|");
+        cats.removeIf(cat -> cat.getColor() == Cat.Color.TABBY);
+        Printer.print(cats);
+
+        System.out.println("|_After removing by name(equal to 5 chars)_|");
+        cats.removeIf(cat -> cat.getName().length() == 5);
+        Printer.print(cats);
+    }
+}
